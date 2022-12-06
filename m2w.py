3b9bd9589a5d075d3d53de9f8b8e5a13dc13cef7
huangwb8 <654751191@qq.com>
2022-12-06 11:13:45 +0800

    Update m2w.py

--- m2w.py
+++ m2w.py
@@ -38,17 +38,16 @@ if __name__ == '__main__':
         path_legacy_json = path_m2w + website['path_legacy_json'] + '_' + i + '.json'
 
         # Connect the WordPress website
-        print('========')
+        print('========Website: ' + i)
         client = wp_xmlrpc(domain, username, password)
 
-        # Gather paths of brand-new and changed legacy markdown files
+        # # Gather paths of brand-new and changed legacy markdown files
         res = md_detect(path_markdown, path_legacy_json, verbose = True)
         md_upload = res['new']
         md_update = res['legacy']
 
         # Upload or Update
         if len(md_upload) > 0 or len(md_update) > 0:
-            print(i, ': With new or changed legacy markdown files.')
             up(client, md_upload, md_update, post_metadata, verbose = True)
         else:
-            print(i, ': Without any new or changed legacy markdown files. Ignored.')
+            print('Without any new or changed legacy markdown files. Ignored.')
